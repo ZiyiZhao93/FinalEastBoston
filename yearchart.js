bd6f1c3d0f3c56d8e6ddc9fbd97c635a2e9a824f
ZiyiZhao93 <31828737+ZiyiZhao93@users.noreply.github.com>
2018-03-27 22:14:59 -0400

    /

--- yearchart.js
+++ yearchart.js
@@ -1,7 +1,7 @@
 // set the dimensions and margins of the graph
-var margin = {top: 20, right: 20, bottom: 30, left: 230},
+var margin = {top: 20, right: 20, bottom: 30, left: 70},
     width = 500 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    height = 300 - margin.top - margin.bottom;
 
 // set the ranges
 var x = d3.scaleLinear()
@@ -14,44 +14,44 @@ var y = d3.scaleBand()
 // append the svg object to the body of the page
 // append a 'group' element to 'svg'
 // moves the 'group' element to the top left margin
-var svg4 = d3.select("#svg4")
-    .append("svg")
+var svg2 = d3.select("#svg2")
+	  .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-    .append("g")
+  	.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // get the data
-d3.csv("csv/ChurchDenomination.csv", function(error, data) {
+d3.csv("csv/church-year.csv", function(error, data) {
   if (error) throw error;
 
   // format the data
   data.forEach(function(d) {
-    d.number = +d.number;
+    d.founded = +d.founded;
   });
 
 
   // Scale the range of the data in the domains
-  x.domain([0, d3.max(data, function(d) { return +d.number; })]);
-  y.domain(data.map(function(d) { return d.denomination; }));
+  x.domain([2018, d3.max(data, function(d) { return +d.founded; })]);
+  y.domain(data.map(function(d) { return d.name; }));
 
   // append the rectangles for the bar chart
-  svg4.selectAll(".bar")
+  svg2.selectAll(".bar")
       .data(data)
       .enter().append("rect")
       .attr("class", "bar")
-      .attr("width", function(d) { return x(d.number);})
-      .attr("y", function(d) { return y(d.denomination); })
+      .attr("width", function(d) { return x(d.founded);})
+      .attr("y", function(d) { return y(d.name); })
       .attr("height", y.bandwidth())
-      .attr('fill',"lightskyblue");
+      .attr('fill', function(d) {return d.color; });
 
   // add the x Axis
-  svg4.append("g")
+  svg2.append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
 
   // add the y Axis
-  svg4.append("g")
+  svg2.append("g")
       .call(d3.axisLeft(y));
 
 });
