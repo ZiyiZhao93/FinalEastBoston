ddd34e969a5246186d8c2ecce847030acd4f5d42
ZiyiZhao93 <31828737+ZiyiZhao93@users.noreply.github.com>
2018-04-10 08:24:55 -0400

    4.10

--- indexindex.js
+++ indexindex.js
@@ -1,26 +1,12 @@
 var width = document.getElementById('svg0').clientWidth;
 var height = document.getElementById('svg0').clientHeight;
 
-var width2 = document.getElementById('svg1').clientWidth;
-var height2 = document.getElementById('svg1').clientHeight;
-
-var width4 = document.getElementById('svg4').clientWidth;
-var height4 = document.getElementById('svg4').clientHeight;
-
 var marginLeft = 0;
 var marginTop = 0;
 
 var clicked = true;
 
 var svg = d3.select('#svg0')
-    .append('g')
-    .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
-
-var svg1 = d3.select('#svg1')
-    .append('g')
-    .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
-
-var svg4 = d3.select('#svg4')
     .append('g')
     .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
 
@@ -41,7 +27,28 @@ queue()
     
     .await(function (err, circleData, mapData, streetData) {
 
-        svg4.selectAll("circle")
+
+        svg.selectAll("path")
+            .data(mapData.features)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .attr("class", "backcolor")
+            .attr("stroke", "lightgray")
+            .attr("stroke-width", 1)
+            .attr("fill", "aliceblue");
+
+        svg.selectAll("path")
+            .data(streetData.features)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .attr("class", "street")
+            .attr("stroke", "lightgray")
+            .attr("stroke-width", 1)
+            .attr("fill", "none");
+
+        svg.selectAll("circle")
             .data(circleData.features)
             .enter()
             .append("path")
@@ -56,29 +63,6 @@ queue()
             .on('mouseout', function (d) {
                 d3.select(this).attr("fill", "skyblue");;
             });
-        
-
-        svg.selectAll("path")
-            .data(mapData.features)
-            .enter()
-            .append("path")
-            .attr("d", path)
-            .attr("class", "backcolor")
-            .attr("stroke", "lightgray")
-            .attr("stroke-width", 1)
-            .attr("fill", "aliceblue");
-
-        svg1.selectAll("path")
-            .data(streetData.features)
-            .enter()
-            .append("path")
-            .attr("d", path)
-            .attr("class", "street")
-            .attr("stroke", "lightgray")
-            .attr("stroke-width", 1)
-            .attr("fill", "none");
-
-
 
     });
 
